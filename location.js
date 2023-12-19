5721b434ed2a9a83392b286d242720ab70ffe14f
memasuuuuuuuur <154038109+memasuuuuuuuur@users.noreply.github.com>
2023-12-19 16:15:58 +0800

    location.js

---
+++ location.js
@@ -0,0 +1,91 @@
+function loginnginamo(){
+    window.location.href = "http://localhost/login.php"
+  }
+  
+  function aboutMeHome(){
+    window.location.href = "http://localhost/third-about-me.html"
+  }
+  
+  function registerButton(){
+    window.location.href = "http://localhost/signup.html"
+  }
+
+  function contactHome(){
+    window.location.href = "http://localhost/contact.html"
+  }
+  
+  function infoHome(){
+    window.location.href = "http://localhost/info.html"
+  }
+  function homeInLogin(){
+    window.location.href = "http://localhost/homepage.html"
+  }
+  function register(){
+    window.location.href = "http://localhost/second.html";
+  }
+  
+  function login(){
+    window.location.href = "http://localhost/first.html";
+  }
+  
+  function information(){
+    window.location.href = "http://localhost/third-about-me.html"
+  }
+  
+  function americano(){
+    window.location.href = "http://localhost/drink1.html"
+  }
+  
+  function choices(){
+    window.location.href = "http://localhost/fourth.html"
+  }
+  
+  function cortado(){
+    window.location.href = "http://localhost/drink2.html"
+  }
+  
+  function mocha(){
+    window.location.href = "http://localhost/drink3.html"
+  }
+  
+  function macchiato(){
+    window.location.href = "http://localhost/drink4.html"
+  }
+  
+  function flatwhite(){
+    window.location.href = "http://localhost/drink5.html"
+  }
+  
+  function decaf(){
+    window.location.href = "http://localhost/drink6.html"
+  }
+  
+  function irish(){
+    window.location.href = "http://localhost/drink7.html"
+  }
+  
+  function iced(){
+    window.location.href = "http://localhost/drink8.html"
+  }
+  
+  function cafe(){
+    window.location.href = "http://localhost/drink9.html"
+  }
+  
+  function coldbrew(){
+    window.location.href = "http://localhost/drink10.html"
+  }
+  
+  function drip(){
+    window.location.href = "http://localhost/drink11.html"
+  }
+  
+  function instant(){
+    window.location.href = "http://localhost/drink12.html"
+  }
+  
+  function seemore(){
+    window.location.href = "https://www.decafco.com.au/blogs/news/the-history-of-decaf-coffee"
+  }
+  
+  
